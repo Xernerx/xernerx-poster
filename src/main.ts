e03074d403e42b44aed8e568330b8081fa7e349d
Dummi <raydecrom@hotmail.com>
2023-02-21 20:48:56 +0100

    Initial release

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,39 @@
-class XernerxPoster {
-    name: string;
-    client: Client;
+import PostStats from './models/stats.js';
 
-    constructor(client: Client) {
-        this.name = "XernerxPoster";
+export default class XernerxPoster {
+    public name;
+    public client;
+    public options;
+
+    constructor(client: Record<string, unknown>, options: Options) {
+        this.name = 'XernerxPoster';
 
         this.client = client;
+
+        this.options = options;
+
+        PostStats(options.topgg, this.client as unknown as Client);
+
+        setInterval(async () => {
+            PostStats(options.topgg, this.client as unknown as Client);
+        }, 1800000);
     }
 }
 
-interface Client {
+interface Options {
+    topgg: string;
+}
 
-};
+export interface Client {
+    guilds: Record<string, Function>;
+    options: {
+        shardCount: number;
+        shards: Array<number>;
+    };
+    ws: {
+        totalShards: number;
+    };
+    emit: Function;
+}
 
-export { XernerxPoster }
+export { XernerxPoster };
--- src/models/stats.ts
+++ src/models/stats.ts
@@ -1,40 +1,23 @@
-import got from 'got';
+import { Client } from '../main.js';
 
-interface Client {
-    token: string,
-    user: {
-        id: string,
-    },
-    guilds: Record<string, Function | object>
-    [index: string]: unknown
-}
-export class Post {
-    client: Client;
+export default async function PostStats(token: string, client: Client) {
+    const baseUrl = 'https://top.gg/api/';
 
-    constructor(client: Client) {
-        this.client = client;
-    }
+    const body = {
+        server_count: (await client.guilds.fetch()).size,
+        shard_id: client.options.shards[0] || 0,
+        shard_count: client.options.shardCount || client.ws.totalShards || client.options.shards.length,
+    };
 
-    botsForDiscord(token: string) {
-        try {
-            (async () => {
-                const response = await got.post(`https://discords.com/bots/api/bot/${this.client.user.id}`, {
-                    json: {
-                        headers: {
-                            Authorization: token
-                        },
-                        body: {
-                            server_count: (await (this.client.guilds.fetch as Function)()).size
-                        }
-                    }
-                })
+    const res = await fetch(`${baseUrl}bots/stats`, {
+        method: 'POST',
+        headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    });
 
-                console.log(response)
-            })();
-        }
-
-        catch (error) {
-            // this.client.emit()
-        }
-    }
+    if (res.status === 200) return client.emit('webhookPost', client, body);
+    else if (!res.ok) client.emit('webhookError', client, body, (await res.json()).error);
 }
